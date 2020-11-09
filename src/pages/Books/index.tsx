1f81dcb27f425301afa9f813789af6f1b9f289a4
Marcos Paulo <marcosp.mail@gmail.com>
2020-11-09 15:14:27 -0300

    Fix: Bug in paginator pages number

--- src/pages/Books/index.tsx
+++ src/pages/Books/index.tsx
@@ -134,7 +134,7 @@ const Books: React.FC = () => {
   }, [favorites])
 
   const calculateMaxPages = useCallback(() => {
-    const PAGE_DIVIDER = 80
+    const PAGE_DIVIDER = 60
     const MAX_PAGES = 15
 
     const maxPages = Math.min(window.innerWidth / PAGE_DIVIDER, MAX_PAGES)
@@ -155,6 +155,8 @@ const Books: React.FC = () => {
     let start = currentPage - Math.floor(length / 2)
     start = Math.max(start, min)
     start = Math.min(start, min + total - length)
+
+    start = Math.floor(start)
 
     const pages = Array.from({ length }, (el, i) => start + i)
     setPages(pages)
